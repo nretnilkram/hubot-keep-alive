3e6268f09d642b2cd29433c2d1c98e6d4851d33b
Mark Lintern <mark.lintern@greenlight.guru>
2025-12-10 16:47:25 -0500

    Update Package versions and convert to JS

---
+++ index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (robot, scripts) => {
+  const scriptsPath = path.resolve(__dirname, 'src');
+  fs.access(scriptsPath, fs.constants.F_OK, (err) => {
+    if (!err) {
+      const scriptFiles = fs.readdirSync(scriptsPath);
+      for (const script of scriptFiles) {
+        if (scripts && !scripts.includes('*')) {
+          if (scripts.includes(script)) {
+            robot.loadFile(scriptsPath, script);
+          }
+        } else {
+          robot.loadFile(scriptsPath, script);
+        }
+      }
+    }
+  });
+};
---
+++ src/keep-alive.js
@@ -0,0 +1,53 @@
+// Description
+//   A hubot script that keeps it's sleepy web apps alive.
+//
+// Notes:
+//   This will help you keep your hubot alive if it lives on a
+//   server that will go to sleep when dormant.
+//
+// Configuration:
+//   HUBOT_KEEP_ALIVE_URL: required
+//   HUBOT_KEEP_ALIVE_CRON: optional, defaults to every 5 minutes from 6am to 10pm
+//   TZ: optional, defaults to "UTC"
+//
+// URLs:
+//   GET /heroku/keepalive
+//
+
+module.exports = (robot) => {
+  const { CronJob } = require('cron');
+  const keepAliveUrl = process.env.HUBOT_KEEP_ALIVE_URL || 'http://localhost:8080/keepalive/ping';
+  const cronSchedule = process.env.HUBOT_KEEP_ALIVE_CRON || '0 */5 6-22 * * *';
+  const timezone = process.env.TZ || 'UTC';
+
+  robot.logger.info('keepAliveUrl: ', keepAliveUrl);
+  robot.logger.info('cronSchedule: ', cronSchedule);
+  robot.logger.info('timzone: ', timezone);
+
+  const keepAlive = (robot) => {
+    robot.http(keepAliveUrl).get()((err, response, body) => {
+      if (err) {
+        robot.logger.error(err);
+      } else {
+        robot.logger.info('Hubot keep alive check successful.');
+      }
+    });
+  };
+
+  const keepAliveResponse = (req, res) => {
+    res.set('Content-Type', 'text/plain');
+    res.send('PONG');
+  };
+
+  // Bind /keepalive/ping endpoint
+  robot.router.get("/keepalive/ping", keepAliveResponse);
+
+  // Create CronJob
+  try {
+    new CronJob(cronSchedule, () => {
+      keepAlive(robot);
+    }, null, true, timezone);
+  } catch (e) {
+    console.log(e);
+  }
+};
